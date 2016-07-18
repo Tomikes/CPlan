0ce853ec433af0792b8d9b824566579eb132038e
Tomikes <mike_shang@outlook.com>
2016-07-18 08:58:28 +0800

    Initial Commit

---
+++ CPlan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPlan
+//
+//  Created by mike on 7/18/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
